b62f7e2e50964107d49663a72b863513b2d34776
Christian Muñoz <chgeovany.15@gmail.com>
2022-10-12 22:28:34 -0500

    Cambiar fondo

--- Tareas/tarea07_cambiar_fondo/CambiarFondo.py
+++ Tareas/tarea07_cambiar_fondo/CambiarFondo.py
@@ -1,27 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import cv2
 import numpy as np
 
-# Video de entrada
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-fondo=cv2.imread('E:\\UDG Kevin\\Semestre 3\\Sem. Algoritmia\\Programas\\Cambiar Fondo\\1.jpg')
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+size = (frame_width, frame_height)
+result = cv2.VideoWriter('final.avi',
+						cv2.VideoWriter_fourcc(*'MJPG'),
+						10, size)
+fondo=cv2.imread('./fondo1.jpg')
 imagen_s=cv2.resize(fondo,(640,480))
 while (True):
         ret, frame = cap.read()
         if ret ==False:
             break
         
-        bgr = [30,204,73]
-        thresh = 95   
-
-        minBGR = np.array([bgr[0] - thresh, bgr[1] - thresh, bgr[2] - thresh])
-        maxBGR = np.array([bgr[0] + thresh, bgr[1] + thresh, bgr[2] + thresh])
+        bgr = [97,181,147]
+        trs = 50
+        
+        minBGR = np.array([bgr[0]-trs,bgr[1]-trs,bgr[2]-trs])
+        maxBGR = np.array([bgr[0]+trs,bgr[1]+trs,bgr[2]+trs])
         maskBGR = cv2.inRange(frame,minBGR,maxBGR)
         mask_inv = cv2.bitwise_not(maskBGR)
         cv2.imshow('mascara',maskBGR)
@@ -37,9 +35,20 @@ while (True):
         cv2.imshow('Fondo', imagen_s)
 
         cv2.imshow('frame',frame)
-
+        
+        if cv2.waitKey(1) & 0xFF==ord('c'):
+            fondo=cv2.imread('./fondo2.jpg')
+            imagen_s=cv2.resize(fondo,(640,480))
+        
+        if cv2.waitKey(1) & 0xFF==ord('b'):
+            fondo=cv2.imread('./fondo1.jpg')
+            imagen_s=cv2.resize(fondo,(640,480))
+        
+        result.write(total)
+            
         if cv2.waitKey(1) & 0xFF==ord('q'):
             break
         
 cap.release()
+result.release()
 cv2.destroyAllWindows()
