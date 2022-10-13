0e38c4d600b4a5b37505049753a42e6a9ea611fd
Christian_Windows_Desktop <chgeovany.15@gmail.com>
2022-10-12 23:09:47 -0500

    Se cambiaron las variables

--- Tareas/tarea07_cambiar_fondo/CambiarFondo.py
+++ Tareas/tarea07_cambiar_fondo/CambiarFondo.py
@@ -1,54 +1,54 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
+video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+frame_width = int(video.get(3))
+frame_height = int(video.get(4))
 size = (frame_width, frame_height)
-result = cv2.VideoWriter('final.avi',
+exportable = cv2.VideoWriter('final.avi',
 						cv2.VideoWriter_fourcc(*'MJPG'),
 						10, size)
 fondo=cv2.imread('./fondo1.jpg')
-imagen_s=cv2.resize(fondo,(640,480))
+fondo=cv2.resize(fondo,(640,480))
 while (True):
-        ret, frame = cap.read()
+        ret, frame = video.read()
         if ret ==False:
             break
         
         bgr = [97,181,147]
         trs = 50
         
-        minBGR = np.array([bgr[0]-trs,bgr[1]-trs,bgr[2]-trs])
-        maxBGR = np.array([bgr[0]+trs,bgr[1]+trs,bgr[2]+trs])
-        maskBGR = cv2.inRange(frame,minBGR,maxBGR)
-        mask_inv = cv2.bitwise_not(maskBGR)
-        cv2.imshow('mascara',maskBGR)
+        low = np.array([bgr[0]-trs,bgr[1]-trs,bgr[2]-trs])
+        high = np.array([bgr[0]+trs,bgr[1]+trs,bgr[2]+trs])
+        mask = cv2.inRange(frame,low,high)
+        mask_inv = cv2.bitwise_not(mask)
+        cv2.imshow('mascara',mask)
         cv2.imshow('mascara_inv',mask_inv)
 
-        resultBGR = cv2.bitwise_and(frame,frame, mask=mask_inv)
+        result = cv2.bitwise_and(frame,frame, mask=mask_inv)
 
-        result_inv = cv2.bitwise_and(imagen_s, imagen_s, mask = maskBGR)
+        result_inv = cv2.bitwise_and(fondo, fondo, mask = mask)
 
-        total=cv2.add(resultBGR,result_inv)
+        total=cv2.add(result,result_inv)
 
-        cv2.imshow('resultado',total)
-        cv2.imshow('Fondo', imagen_s)
+        cv2.imshow('Final',total)
+        cv2.imshow('Fondo', fondo)
 
-        cv2.imshow('frame',frame)
+        cv2.imshow('Captura',frame)
         
         if cv2.waitKey(1) & 0xFF==ord('c'):
             fondo=cv2.imread('./fondo2.jpg')
-            imagen_s=cv2.resize(fondo,(640,480))
+            fondo=cv2.resize(fondo,(640,480))
         
         if cv2.waitKey(1) & 0xFF==ord('b'):
             fondo=cv2.imread('./fondo1.jpg')
-            imagen_s=cv2.resize(fondo,(640,480))
+            fondo=cv2.resize(fondo,(640,480))
         
-        result.write(total)
+        exportable.write(total)
             
         if cv2.waitKey(1) & 0xFF==ord('q'):
             break
         
-cap.release()
-result.release()
+video.release()
+exportable.release()
 cv2.destroyAllWindows()
